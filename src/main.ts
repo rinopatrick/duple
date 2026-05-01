1cbc622d7e7b00de22024ed1d60ed30fbaff4600
patrickrinoh0910 <patrickrinoh0910@gmail.com>
2026-05-01 22:28:27 +0700

    Fix: unregister old service worker on startup to prevent stale cache

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 
+// Unregister any old service worker that may be caching stale files
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    regs.forEach(r => r.unregister());
+  });
+}
+
 mount(App, { target: document.getElementById('app')! })
 
 export default {}
